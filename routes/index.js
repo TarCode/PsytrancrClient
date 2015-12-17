1c4cddf08053b2f43baced3d47461f02290c8692
TarCode <taariq@projectcodex.co>
2015-12-17 23:25:10 +0200

    restyled interface, added single event and landing page

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,21 @@ var request = require('request');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    res.render('land');
+});
+
+router.get('/events', function(req, res, next) {
     getEvents(function(err, response, body) {
       var data = JSON.parse(body);
       res.render('index', {events:data});
+    });
+});
+
+router.get('/events/:id', function(req, res, next) {
+    var eventId = req.params.id;
+    getEventById(eventId, function(err, response, body) {
+      var data = JSON.parse(body);
+      res.render('event', {event:data[0], artists: data[1]});
     });
 });
 
@@ -14,4 +26,8 @@ module.exports = router;
 
 function getEvents(cb){
   request('http://localhost:3000/api/eventList', cb);
+}
+
+function getEventById(eventId, cb){
+  request('http://localhost:3000/api/event/'+ eventId, cb);
 }
